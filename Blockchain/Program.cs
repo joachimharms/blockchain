6fa19012613036e51a54e848d7698ae0dd188140
joachimharms <joachimharms77@gmail.com>
2017-11-30 22:45:20 +0100

    Initial commit

---
+++ Blockchain/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+using NBitcoin;
+
+namespace Blockchain
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Key privateKey = new Key();
+
+            // From the private key, we use a one-way cryptographic function, to generate a public key.
+            PubKey publicKey = privateKey.PubKey;
+            Console.WriteLine(publicKey);
+
+            // Get your addresses from your public key
+            Console.WriteLine(publicKey.GetAddress(Network.Main));
+            Console.WriteLine(publicKey.GetAddress(Network.TestNet));
+
+            var publicKeyHash = publicKey.Hash;
+
+        }
+    }
+}
